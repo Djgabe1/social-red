f540ddd05b53f2d179d0b9ea59fa8755ef5583e3
Djgabe1 <deissygarciabeltran@gmail.com>
2021-11-28 14:10:32 -0500

    Cambios basicos

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postsRoute = require('./routes/posts');
+const multer = require('multer');
+const path = require('path')
 
 dotenv.config();
 //conexion Base de datos
@@ -14,10 +16,31 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
     console.log('Conneted to MongoDB');
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //Middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+//Archives
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'public/images');
+    },
+    filename: (req,file,cb)=>{ 
+        cb(null, req.body.name); 
+    }
+})
+
+const upload = multer({storage});
+app.post('/api/upload', upload.single('file'), (req,res)=>{
+    try {
+        return res.status(200).json('File uploaded successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 //routes
 app.use('/api/users', userRoute);
